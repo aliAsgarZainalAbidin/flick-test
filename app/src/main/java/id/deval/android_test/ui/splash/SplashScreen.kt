bfbca6342ac675ee27aadfafd837eaa5f30cde92
Ali Asgar Zainal Abidin <49857639+aliAsgarZainalAbidin@users.noreply.github.com>
2021-08-11 07:09:42 +0800

    add splashscreen and nav component

--- app/src/main/java/id/deval/android_test/ui/splash/SplashScreen.kt
+++ app/src/main/java/id/deval/android_test/ui/splash/SplashScreen.kt
@@ -1,12 +1,23 @@
 package id.deval.android_test.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import id.deval.android_test.MainActivity
 import id.deval.android_test.R
+
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
