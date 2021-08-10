b8cb3f21a7e2c8af14b6070800677853de7de3d4
Ali Asgar Zainal Abidin <49857639+aliAsgarZainalAbidin@users.noreply.github.com>
2021-08-11 06:55:23 +0800

    add ui tabmenu,style,icon

--- app/src/main/java/id/deval/android_test/MainActivity.kt
+++ app/src/main/java/id/deval/android_test/MainActivity.kt
@@ -2,10 +2,29 @@ package id.deval.android_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import id.deval.android_test.adapter.ViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private val LIST_TAB = arrayListOf<String>("User", "Repository", "Issue")
+    private lateinit var viewpager: ViewPager2
+    private lateinit var tabLayout: TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewpager = findViewById(R.id.vp_main_container)
+        tabLayout = findViewById(R.id.tablayout_main_container)
+
+        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+        viewpager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewpager) { tab, position ->
+            tab.text = LIST_TAB[position]
+        }.attach()
     }
 }
---
+++ app/src/main/java/id/deval/android_test/adapter/ViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package id.deval.android_test.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import id.deval.android_test.ui.tab.repository.RepositoryFragment
+import id.deval.android_test.ui.tab.user.UserFragment
+
+private val NUM_TABS = 3
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return NUM_TABS
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            0 -> return UserFragment()
+            1 -> return RepositoryFragment()
+            2 -> return RepositoryFragment()
+        }
+        return UserFragment()
+    }
+
+}
